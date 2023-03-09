2d563d5d7931c4fecab0d1c179916552d5807678
nokuL <lunganokuthaba@gmail.com>
2023-03-09 18:32:37 +0200

    TemperatureCloseToZero

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 
 public class Main {
     public static void main(String[] args) throws Exception {
-       int [] temps = new int[]{-1,2, -3, -4};
+       int [] temps = new int[]{-4,-2,1,4,8};
         System.out.println(TemperatureCloseToZero.temperature(temps));
 
     }
--- src/TemperatureCloseToZero.java
+++ src/TemperatureCloseToZero.java
@@ -8,13 +8,15 @@ public class TemperatureCloseToZero {
         }
         int closest = temps[0];
         for (int i = 1; i < temps.length; i++) {
-           closest = Math.min(Math.abs(temps[1]), Math.abs(closest));
-           if(temps[i]< 0 && temps[i]== (-1*closest)){
+            closest = Math.min(Math.abs(closest), Math.abs(temps[i]));
+              if(temps[i]< 0 && temps[i]== (-1*closest)){
                closest = temps[i];
            }
            if(Math.abs(temps[i])== Math.abs(closest)){
                closest = Math.max(closest, temps[i]);
            }
+
+
         }
         return closest;
 
