1cef40d7290fd2318b60d7de98ef11399edb90e6
nokuL <lunganokuthaba@gmail.com>
2024-06-25 11:35:28 +0200

    find middle noe

---
+++ src/LinkedLists/FindMiddleNode.java
@@ -0,0 +1,20 @@
+package LinkedLists;
+
+public class FindMiddleNode<T> {
+
+    public Node<T>findMiddleNode(LinkedList<T>linkedList){
+        Node<T>fast = linkedList.getHead();
+        Node<T>slow = linkedList.getHead();
+
+        while(fast != null){
+            fast = fast.getNext();
+
+            if(fast!=null){
+                fast = fast.getNext();
+                slow = slow.getNext();
+            }
+        }
+        return slow;
+
+    }
+}
