1d6ae02cad64e35d608eda63741a2f576992e7c0
nokuL <lunganokuthaba@gmail.com>
2024-07-02 12:44:22 +0200

    reverse a linkedlist

--- src/LinearAlgs/FindMiddleNode.java
+++ src/LinearAlgs/FindMiddleNode.java
@@ -2,14 +2,16 @@ package LinearAlgs;
 
 public class FindMiddleNode<T> {
 
-    public Node<T>findMiddleNode(LinkedList<T>linkedList){
-        Node<T>fast = linkedList.getHead();
-        Node<T>slow = linkedList.getHead();
+    public Node<T> findMiddleNode(LinkedList<T> linkedList) {
+        //define two pointers
+        //have one leading fast pointr to go before the slow one
+        //at the end of the loop, the slow loop will be at the middle
 
-        while(fast != null){
+        Node<T> fast = linkedList.getHead();
+        Node<T> slow = linkedList.getHead();
+        while (fast != null) {
             fast = fast.getNext();
-
-            if(fast!=null){
+            if (fast != null) {
                 fast = fast.getNext();
                 slow = slow.getNext();
             }
---
+++ src/LinearAlgs/ReverseLinkedList.java
@@ -0,0 +1,19 @@
+package LinearAlgs;
+
+public class ReverseLinkedList<T> {
+
+    public Node<T>reverseLinkedList(Node<T>node){
+       Node<T>prev = null;
+       Node<T>curr = node;
+       Node<T>next = null;
+       while(curr!=null){
+           next = curr.getNext();
+           curr.setNext(prev);
+           prev = curr;
+           curr = next;
+       }
+       node = prev;
+       return node;
+
+    }
+}
